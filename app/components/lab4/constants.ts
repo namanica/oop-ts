689c72b91c5d6c02c21bb45248d8608cb5e2cc4a
namanica <veronichkapilipna@gmail.com>
2024-12-19 00:56:28 +0200

    synchronized tabbar and menu item chosing and implemented upgraded classes

---
+++ app/components/lab4/constants.ts
@@ -0,0 +1,34 @@
+import { MenuProps } from "antd";
+
+type MenuItem = Required<MenuProps>["items"][number];
+export const items: MenuItem[] = [
+  {
+    label: "Файл",
+    key: "file",
+    children: [
+      { label: "Створити", key: "create" },
+      { label: "Відкрити", key: "open" },
+      { label: "Зберегти", key: "save" },
+      { type: "divider" },
+      { label: "Друк", key: "print" },
+      { type: "divider" },
+      { label: "Вихід", key: "exit" },
+    ],
+  },
+  {
+    label: "Обʼєкти",
+    key: "objects",
+    children: [
+      { label: "Крапка", key: "dot" },
+      { label: "Лінія", key: "line" },
+      { label: "Прямокутник", key: "rectangle" },
+      { label: "Еліпс", key: "ellipse" },
+      { label: "Лінія з кружечками", key: "dumbbell" },
+      { label: "Куб", key: "cube" },
+    ],
+  },
+  {
+    label: "Довідка",
+    key: "note",
+  },
+];
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,8 @@ import { useEffect, useState } from "react";
 // import { Lab1 } from "./components/lab1/Lab1";
 // import { Lab2 } from "./components/lab2/Lab2(func)";
 // import { Lab2 } from "./components/lab2/Lab2(oop)";
-import { Lab3 } from "./components/lab3/Lab3";
+// import { Lab3 } from "./components/lab3/Lab3";
+import { Lab4 } from "./components/lab4/Lab4";
 
 const App = () => {
   const [isClient, setIsClient] = useState(false);
@@ -12,7 +13,7 @@ const App = () => {
     setIsClient(true);
   }, []);
 
-  return <div>{isClient && <Lab3 />}</div>;
+  return <div>{isClient && <Lab4 />}</div>;
 };
 
 export default App;
